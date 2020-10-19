d47881268ffb31f52f23422cfab2349a5ce46f58
Fatih Sevban UYanık <fatihsevban15@gmail.com>
2020-10-19 20:39:52 +0300

    Database seeder implemented

--- PsyclogBackend/seeder/seeder.js
+++ PsyclogBackend/seeder/seeder.js
@@ -1,10 +1,20 @@
 const ClientRequest = require('../model/clientRequest')
+const Chat = require('../model/chat')
+const Message = require('../model/message')
+const Calendar = require('../model/calendar')
+const Wallet = require('../model/wallet')
 const Constants = require('../utils/constants')
 const Review = require('../model/review')
+const Appointment = require('../model/appointment')
 const User = require('../model/user')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const faker = require('faker')
+const { getToday } = require('../utils/util')
+const ForumPost = require('../model/forumPost')
+const ForumTopic = require('../model/forumTopic')
+const PatientNote = require('../model/patientNote')
+
 dotenv.config({ path: './config.env' })
 
 // ============================
@@ -26,17 +36,22 @@ const connectToDatabase = async () => {
 // ============================
 
 const seedUsersData = async () => {
-   for (let i = 0; i < 300; i++) {
+
+   const patients = []
+   const psychologists = [] 
+   const password = '0123456789'
+   const passwordConfirm = '0123456789'
+
+   for (let i = 0; i < 100; i++) {
 
       const username = faker.internet.userName()
       const surname = faker.name.lastName()
       const email = faker.internet.email()
       const name = faker.name.firstName()
-      const password = 'naruto1212'
-      const passwordConfirm = 'naruto1212'
+
        
-      if (i % 2 === 0) {
-         const appointmentPrice = faker.random.number()
+      if (i % 2 === 0) { // seed Psychologist
+         const appointmentPrice = 100
          const biography = faker.lorem.paragraph()
          const profileImage = faker.image.imageUrl()
          const role = Constants.ROLE_PSYCHOLOGIST
@@ -60,75 +75,193 @@ const seedUsersData = async () => {
             isActiveForClientRequest 
          }
          
-         await User.create(data)
+
+         // create calendar for the psychologist
+         const calendar = new Calendar({ role: data.role})
+         data.calendar = calendar._id
+
+         // create wallet for the user
+         const wallet = new Wallet({})
+         data.wallet = wallet._id
+
+         // creating the psychologist.
+         const user = await User.create(data)
+         calendar.user = user
+         wallet.owner = user
+         await calendar.save()
+         await wallet.save()
+         psychologists.push(user)
       }
 
-      else {
-         const cash = faker.random.number()
+      else { // seed Patient
          const role = Constants.ROLE_USER
          const registeredPsychologists = []
          const clientRequests = []
+         const cash = 3000
 
          const data = { 
             username, name, 
             surname, email, 
-            cash, clientRequests, 
+            clientRequests, 
             role, password, 
             registeredPsychologists,
             passwordConfirm 
          }
          
-         await User.create(data)
+
+         // create calendar for the psychologist
+         const calendar = new Calendar({ role: data.role})
+         data.calendar = calendar._id
+
+         // create wallet for the user
+         const wallet = new Wallet({ cash })
+         data.wallet = wallet._id
+
+         // creating the psychologist.
+         const user = await User.create(data)
+         calendar.user = user
+         wallet.owner = user
+         await calendar.save()
+         await wallet.save()
+         patients.push(user)
       }
    }
 
-   const email = 'fatihsevban15@gmail.com'
-   const passwordConfirm = 'naruto1212'
-   const role = Constants.ROLE_ADMIN
-   const password = 'naruto1212'
-   const name = 'fatih sevban'
-   const username = 'fatih15'
-   const surname = 'uyanik'
-   
-   const data = { 
-      username, name, 
-      surname, email, 
-      role, password, 
-      passwordConfirm 
+   const oneDay = 24 * 60 * 60 * 1000
+   let appointmentDate = getToday(true) + 5 * oneDay
+   const intervals = [ 3, 4, 5 ] 
+
+   for (let i = 0; i < 45; i++) {
+      const psychologist = psychologists[i]
+
+      for (let j = i; j < i + 5; j++) {
+         if (i % 2 == 0) {
+            psychologist.patients.push(patients[i]._id)
+            patients[j].registeredPsychologists.push(psychologist)
+            patients[j].passwordConfirm = passwordConfirm
+            patients[j].password = password
+            await patients[j].save()
+            await seedReviewsData(patients[j], psychologist)
+            
+            const price = intervals.length * psychologist.appointmentPrice
+            const appointment = { 
+               psychologist: psychologist._id, 
+               patient: patients[j]._id, 
+               intervals, appointmentDate, price }
+
+            await Appointment.create(appointment)
+            appointmentDate += oneDay
+            
+            const chat = await Chat.create({ 
+               psychologist: psychologist._id, 
+               patient: patients[j]._id, messages: []
+            })
+            
+            let lastMessage 
+
+            for (let k = 0; k < 100; k++) {
+               if (Math.random() < 0.5) {
+                  lastMessage = await Message.create({ 
+                     message: faker.lorem.paragraph(), 
+                     author: psychologist._id,  
+                     contact: patients[j]._id, 
+                     chat: chat._id 
+                  })
+               } else {
+                  lastMessage = await Message.create({ 
+                     message: faker.lorem.paragraph(), 
+                     contact: psychologist._id,  
+                     author: patients[j]._id, 
+                     chat: chat._id 
+                  })
+               }
+            }
+
+            chat.lastMessage = lastMessage
+            await chat.save()
+            
+            for (let k = 0; k < 10; k++) {
+               await PatientNote.create({
+                  psychologist: psychologist._id,
+                  patient: patients[j]._id, 
+                  content: faker.lorem.paragraph()
+               })
+            }
+
+         } 
+         else {
+            await ClientRequest.create({ 
+               content: faker.lorem.paragraph(), 
+               patient: patients[j]._id, 
+               psychologist: psychologist._id 
+            })
+         }
+      }
+
+      psychologist.passwordConfirm = passwordConfirm
+      psychologist.password = password
+      await psychologist.save()      
    }
 
-   await User.create(data)
+   for (let i = 0; i < 50; i++) {
+      const title = faker.name.title()
+      const description = faker.lorem.paragraph()
+      let isAuthorAnonymous = Math.random() < 0.5
+
+      let random = Math.floor(Math.random() * 100)
+      let author = await User.findOne()
+                               .skip(random)
+                               .select('id')
+
+      const topic = await ForumTopic.create({
+         title, description, author, isAuthorAnonymous
+      })
+
+      const messageCount = Math.floor(Math.random() * 100)
+
+      for (let j = 0; j < messageCount; j ++) {
+         const content = faker.lorem.paragraph()
+         random = Math.floor(Math.random() * 100)
+         isAuthorAnonymous = Math.random() < 0.5
+         author = await User.findOne()
+                            .skip(random)
+                            .select('id')
+
+         const images = []
+
+         if (Math.random() < 0.4) {
+            for (let k = 0; k < 3; k++) {
+               images.push('https://picsum.photos/200/300')
+            }
+         }
+
+         await ForumPost.create({
+            topic: topic._id, author: author._id, 
+            isAuthorAnonymous, content, images
+         })
+      }
+   }
 }
 
-const seedRequestsData = async () => {
-   for (let i = 0; i < 20; i++) {
-      const psychologist = await User.findOne({ role: Constants.ROLE_PSYCHOLOGIST })
-      const patient = await User.findOne({ role: Constants.ROLE_USER })
-      const content = faker.lorem.paragraph()
-      const data = { content, patient, psychologist }
-      await ClientRequest.create(data)
-   }
-} 
 
-const seedReviewsData = async () => {
-   for (let i = 0; i < 100; i++) {
+const seedReviewsData = async (author, psychologist) => {
       const rating = 3
       const title = faker.name.title()
       const content = faker.lorem.paragraph()
-      const random = Math.floor(Math.random() * 20)
-      const author = await User.findOne({ role: Constants.ROLE_USER }).skip(random)
-      const psychologist = await User.findOne({ role: Constants.ROLE_PSYCHOLOGIST }).skip(random)
       const data = { title, content, rating, author, psychologist }
-      await Review.create(data)
-   }
+      await Review.create(data) 
 }
+
 
 connectToDatabase()
    .then(result => {
-      seedUsersData()
-      seedReviewsData()
-      seedRequestsData()
-   }).catch(error => {
+      return seedUsersData()
+   })
+
+   .then(() => {
+      console.log('seeded')
+   })
+   .catch(error => {
       console.error(error)
       process.exit()
    })
